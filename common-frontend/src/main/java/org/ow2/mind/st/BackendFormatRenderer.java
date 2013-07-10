ef0fc88df479359ba74ea9851e27923e0d4dba41
Seyvoz Stéphane <stephaneseyvoz@gmail.com>
2013-07-10 18:27:09 +0200

    Revert "Debug-friendly line info in vTables referring to itf files"
    This reverts commit 6a105448650b5ed9529c3cb88a5d57e9260e148a.

--- common-frontend/src/main/java/org/ow2/mind/st/BackendFormatRenderer.java
+++ common-frontend/src/main/java/org/ow2/mind/st/BackendFormatRenderer.java
@@ -99,7 +99,7 @@ public class BackendFormatRenderer implements AttributeRenderer {
     final int i = s.lastIndexOf(':');
     if (i == -1) return "";
     final String inputFilePath = s.substring(0, i);
-    int j = s.indexOf('-', i);
+    final int j = s.indexOf('-', i);
     int lineNumber;
     if (j == -1) {
       try {
@@ -108,14 +108,8 @@ public class BackendFormatRenderer implements AttributeRenderer {
         return "";
       }
     } else {
-      // For interface files the info is formatted as [row,column]
-      String inputFileInfo = s.substring(i + 1, j);
-      if (inputFileInfo.startsWith("[") && inputFileInfo.endsWith("]")) {
-        j = inputFileInfo.indexOf(",");
-        inputFileInfo = inputFileInfo.substring(1, j); // '[' -> ','
-      } // else it should directly be the integer
       try {
-        lineNumber = Integer.parseInt(inputFileInfo);
+        lineNumber = Integer.parseInt(s.substring(i + 1, j));
       } catch (final NumberFormatException e) {
         return "";
       }
